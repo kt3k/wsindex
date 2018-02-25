ee968f9b478d098dd6c313de09c17a69893555a7
Yoshiya Hinosawa <stibium121@gmail.com>
2018-02-25 12:56:52 +0900

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# wsindex v0.1.0
+
+> Web Site Index tool, a static site generator for list of web sites.
+
+wsindex is a static site generator which creates the index web page of the list of web pages.
+
+# Why is this necessary?
+
+If one uses many many static site generators in a project and invokes them simultaneously, then it's a mess to open all of them. This tool works as the index of all the pages and works as the portal of your many many generated static sites.
---
+++ index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+const berber = require('berber')
+const { join } = require('path')
+const pkg = require('./package')
+const through1 = require('through1')
+const layout1 = require('layout1')
+const rename = require('gulp-rename')
+const argv = require('minimist')(process.argv.slice(2))
+const isBuild = argv._ && argv._[0] && argv._[0] === 'build'
+
+const NAME = pkg.name
+const configName = 'package.json'
+
+berber.name(NAME)
+berber.configName('package')
+
+const defaultConfig = {
+  dest: 'build',
+  port: 9000
+}
+
+berber.loggerTitle('wsindex')
+
+berber.on('config', config => {
+  const wsindex = config.wsindex || {}
+  const pages = wsindex.pages || []
+  const data = { pages, isBuild }
+
+  config = Object.assign({}, defaultConfig, wsindex)
+
+  berber
+    .asset(configName)
+    .pipe(through1.obj(file =>
+      Object.assign(file, { data: JSON.parse(file.contents) })))
+    .pipe(layout1.nunjucks(join(__dirname, 'layout.njk'), { data }))
+    .pipe(rename('index.html'))
+
+  berber.dest(config.dest)
+  berber.port(config.port)
+})
